ab7004353e50629c1ae9c96b1b5bdd2aa454b018
ezrajrice <ezra.j.rice@gmail.com>
2015-01-28 12:00:07 -0700

    offboard

--- VBScript/breakdown.vb
+++ VBScript/breakdown.vb
@@ -25,9 +25,45 @@ Dim clawbacks_pending_row As Integer
 Dim jobs_in_jeopardy_row As Integer
 Dim jobs_in_progress_row As Integer
 Dim rep_row As Integer
-dim rate_col as integer
+'Column counters
+dim financial_customer_col as integer
+dim financial_repID_col as integer
+dim financial_jobID_col as integer
+dim financial_payment_col as integer
+dim financial_kW_col as integer
+	dim current_customer_col as integer
+	dim current_jobID_col as integer
+	dim current_systemsize_col as integer
+	dim current_status_col as integer
+	dim current_rep_col as integer
+	dim current_jeopardy_col as integer
+	dim current_cancellation_col as integer
+	dim current_installed_col as integer
+	dim current_inprogress_col as integer
+		dim repList_rep_col as integer
+		dim repList_repname_col as integer
+		dim repList_repID_col as integer
+		dim repList_rate_col as integer
 
-rate_col = 5
+set financial_customer_col = 3
+set financial_jobID_col = 7
+set financial_kW_col = 4
+set financial_repID_col = 2
+set financial_payment_col = 5
+	set current_customer_col = 1
+	set current_jobID_col = 2
+	set current_systemsize_col = 3
+	set current_status_col = 4
+	set current_rep_col = 17
+	set current_jeopardy_col = 19
+	set current_cancellation_col = 20
+	set current_installed_col = 21
+	set current_inprogress_col = 22
+		set repList_rate_col = 5
+		set repList_rep_col = 2
+		set repList_repname_col = 1
+		set repList_repID_col = 3
+
 rep_row = 2
 Do
 'Creates the new tabs
@@ -47,94 +83,102 @@ Do
     clawbacks_pending_row = 3
     jobs_in_jeopardy_row = 3
     jobs_in_progress_row = 3
-	      
-    rep = Sheets("RepList").Cells(rep_row, 2)
-    rep_name = Sheets("RepList").Cells(rep_row, 1)
-	rep_ID = sheets("RepList").cells(rep_row, 3)
-	rate = Sheets("RepList").cells(rep_row, 4)
+
+    rep = Sheets("RepList").Cells(rep_row, repList_rep_col)
+    rep_name = Sheets("RepList").Cells(rep_row, repList_repname_col)
+	rep_ID = sheets("RepList").cells(rep_row, repList_repID_col)
+	rate = Sheets("RepList").cells(rep_row, repList_rate_col)
     
     'cycles through the accounts
     Do
         If Sheets("Current Data").Cells(current_data_row, 17) = rep Then
         
-        'assigns values to the variables reported on
-        job_id = Sheets("Current Data").Cells(current_data_row, 2)
-        customer = Sheets("Current Data").Cells(current_data_row, 1)
-        system_size = Sheets("Current Data").Cells(current_data_row, 3)
-        
-        job_in_jeopardy = Sheets("Current Data").Cells(current_data_row, 19)
-        cancellation = Sheets("Current Data").Cells(current_data_row, 20)
-        installed = Sheets("Current Data").Cells(current_data_row, 21)
-        in_progress = Sheets("Current Data").Cells(current_data_row, 22)
-        
-        status = Sheets("Current Data").Cells(current_data_row, 4)
+			'assigns values to the variables reported on
+			job_id = Sheets("Current Data").Cells(current_data_row, 2)
+			customer = Sheets("Current Data").Cells(current_data_row, 1)
+			system_size = Sheets("Current Data").Cells(current_data_row, 3)
+			
+			job_in_jeopardy = Sheets("Current Data").Cells(current_data_row, 19)
+			cancellation = Sheets("Current Data").Cells(current_data_row, 20)
+			installed = Sheets("Current Data").Cells(current_data_row, 21)
+			in_progress = Sheets("Current Data").Cells(current_data_row, 22)
+			
+			status = Sheets("Current Data").Cells(current_data_row, 4)
 
-        'code for calculating earned based on rep. This is in here for confidentiality.
-		with sheets("Financial Data")
-			do until (IsEmpty(.cells(financial_data_row, 2)))
-				if .cells(financial_data_row, 2) = rep_ID AND .cells(financial_data_row, 3) = customer then
-					earned = system_size * sheets("RepList").cells(rep_row, rate_col)
-					financial_data_row = financial_data_row + 1
+			'code for calculating earned based on rep.
+			with sheets("Financial Data")
+				do until (IsEmpty(.cells(financial_data_row, 2)))
+					if .cells(financial_data_row, 2) = rep_ID AND .cells(financial_data_row, 3) = customer then
+						earned = system_size * sheets("RepList").cells(rep_row, rate_col)
+						financial_data_row = financial_data_row + 1
+					Else
+						financial_data_row = financial_data_row + 1
+					End if
+				Loop
+			end with
+        
+			'code for summing the amount paid for the account
+			Dim sale_amount_row As Integer
+			sale_amount_row = 2
+        
+			Do
+				If Sheets("Financial Data").Cells(sale_amount_row, 2) = rep_name AND sheets("Financial Data").cells(sale_amount_row, 7) = job_id Then
+					sale_amount = sale_amount + Sheets("Financial Data").Cells(sale_amount_row, 5)
+					sale_amount_row = sale_amount_row + 1
 				Else
-					financial_data_row = financial_data_row + 1
-				End if
-			Loop
-		end with
-        
-        'code for summing the amount paid for the account
-        Dim sale_amount_row As Integer
-        sale_amount_row = 2
-        
-        Do
-            If Sheets("Financial Data").Cells(sale_amount_row, 2) = rep_name AND sheets("Financial Data").cells(sale_amount_row, 7) = job_id Then
-                sale_amount = sale_amount + Sheets("Financial Data").Cells(sale_amount_row, 5)
-                sale_amount_row = sale_amount_row + 1
-            Else
-                sale_amount_row = sale_amount_row + 1
-            End If
-        Loop Until Sheets("Financial Data").Cells(sale_amount_row, 3) = ""
+					sale_amount_row = sale_amount_row + 1
+				End If
+			Loop Until Sheets("Financial Data").Cells(sale_amount_row, 3) = ""
 
-        'Determines which reporting sheet the account goes to
-        If installed = "TRUE" Or installed = "True" Then
-            Sheets("Commissions Earned").Cells(commissions_earned_row, 1) = customer
-            Sheets("Commissions Earned").Cells(commissions_earned_row, 2) = job_id
-            Sheets("Commissions Earned").Cells(commissions_earned_row, 3) = system_size
-            Sheets("Commissions Earned").Cells(commissions_earned_row, 4) = earned
-            Sheets("Commissions Earned").Cells(commissions_earned_row, 5) = sale_amount
-            Sheets("Commissions Earned").Cells(commissions_earned_row, 6) = earned - sale_amount
+			'Determines which reporting sheet the account goes to
+			If installed = "TRUE" Or installed = "True" Then
+				with sheets("Commissions Earned")
+					.Cells(commissions_earned_row, 1) = customer
+					.Cells(commissions_earned_row, 2) = job_id
+					.Cells(commissions_earned_row, 3) = system_size
+					.Cells(commissions_earned_row, 4) = earned
+					.Cells(commissions_earned_row, 5) = sale_amount
+					.Cells(commissions_earned_row, 6) = earned - sale_amount
+				end with
+				
+				commissions_earned_row = commissions_earned_row + 1
+			ElseIf cancellation = "TRUE" Or cancellation = "True" Then
+				with sheets("Clawbacks Pending")
+					.Cells(clawbacks_pending_row, 1) = customer
+					.Cells(clawbacks_pending_row, 2) = job_id
+					.Cells(clawbacks_pending_row, 3) = system_size
+					.Cells(clawbacks_pending_row, 4) = earned
+					.Cells(clawbacks_pending_row, 5) = sale_amount
+					.Cells(clawbacks_pending_row, 6) = earned - sale_amount
+				end with
+				
+				clawbacks_pending_row = clawbacks_pending_row + 1
+			ElseIf job_in_jeopardy = "TRUE" Or job_in_jeopardy = "True" Then
+				with sheets("Jobs in Jeopardy")
+					.Cells(jobs_in_jeopardy_row, 1) = customer
+					.Cells(jobs_in_jeopardy_row, 2) = job_id
+					.Cells(jobs_in_jeopardy_row, 3) = system_size
+					.Cells(jobs_in_jeopardy_row, 4) = status
+					.Cells(jobs_in_jeopardy_row, 5) = sale_amount
+					.Cells(jobs_in_jeopardy_row, 6) = 0 - sale_amount
+				end with
             
-            commissions_earned_row = commissions_earned_row + 1
-        ElseIf cancellation = "TRUE" Or cancellation = "True" Then
-            Sheets("Clawbacks Pending").Cells(clawbacks_pending_row, 1) = customer
-            Sheets("Clawbacks Pending").Cells(clawbacks_pending_row, 2) = job_id
-            Sheets("Clawbacks Pending").Cells(clawbacks_pending_row, 3) = system_size
-            Sheets("Clawbacks Pending").Cells(clawbacks_pending_row, 4) = earned
-            Sheets("Clawbacks Pending").Cells(clawbacks_pending_row, 5) = sale_amount
-            Sheets("Clawbacks Pending").Cells(clawbacks_pending_row, 6) = earned - sale_amount
+				jobs_in_jeopardy_row = jobs_in_jeopardy_row + 1
+			ElseIf in_progress = "TRUE" Or in_progress = "True" Then
+				with sheets("Jobs in Progress")
+					.Cells(jobs_in_progress_row, 1) = customer
+					.Cells(jobs_in_progress_row, 2) = job_id
+					.Cells(jobs_in_progress_row, 3) = system_size
+					.Cells(jobs_in_progress_row, 4) = earned
+					.Cells(jobs_in_progress_row, 5) = sale_amount
+					.Cells(jobs_in_progress_row, 6) = earned - sale_amount
+				end with
             
-            clawbacks_pending_row = clawbacks_pending_row + 1
-        ElseIf job_in_jeopardy = "TRUE" Or job_in_jeopardy = "True" Then
-            Sheets("Jobs in Jeopardy").Cells(jobs_in_jeopardy_row, 1) = customer
-            Sheets("Jobs in Jeopardy").Cells(jobs_in_jeopardy_row, 2) = job_id
-            Sheets("Jobs in Jeopardy").Cells(jobs_in_jeopardy_row, 3) = system_size
-            Sheets("Jobs in Jeopardy").Cells(jobs_in_jeopardy_row, 4) = status
-            Sheets("Jobs in Jeopardy").Cells(jobs_in_jeopardy_row, 5) = sale_amount
-            Sheets("Jobs in Jeopardy").Cells(jobs_in_jeopardy_row, 6) = 0 - sale_amount
-            
-            jobs_in_jeopardy_row = jobs_in_jeopardy_row + 1
-        ElseIf in_progress = "TRUE" Or in_progress = "True" Then
-            Sheets("Jobs in Progress").Cells(jobs_in_progress_row, 1) = customer
-            Sheets("Jobs in Progress").Cells(jobs_in_progress_row, 2) = job_id
-            Sheets("Jobs in Progress").Cells(jobs_in_progress_row, 3) = system_size
-            Sheets("Jobs in Progress").Cells(jobs_in_progress_row, 4) = earned
-            Sheets("Jobs in Progress").Cells(jobs_in_progress_row, 5) = sale_amount
-            Sheets("Jobs in Progress").Cells(jobs_in_progress_row, 6) = earned - sale_amount
-            
-            jobs_in_progress_row = jobs_in_progress_row + 1
-        End If
-            current_data_row = current_data_row + 1
+				jobs_in_progress_row = jobs_in_progress_row + 1
+			End If
+				current_data_row = current_data_row + 1
         Else
-            current_data_row = current_data_row + 1
+				current_data_row = current_data_row + 1
         End If
         
     Loop Until Sheets("Current Data").Cells(current_data_row, 2) = ""
@@ -161,6 +205,7 @@ Do
     ActiveWorkbook.Close
     
     rep_row = rep_row + 1
+	
 Loop Until Sheets("RepList").Cells(rep_row, 2) = ""
 
 End Sub
@@ -197,3 +242,34 @@ Worksheets.Add(, Worksheets(Worksheets.Count)).Name = sheetName
 	
 End Sub
 
+sub email(ByVal rep as string)
+
+	'Email portion of the code
+		Dim OutApp As Object
+		Dim OutMail As Object
+
+		Set OutApp = CreateObject("Outlook.Application")
+		Set OutMail = OutApp.CreateItem(0)
+
+		On Error Resume Next
+	   
+		With OutMail
+			.To = Rep
+			.CC = ""
+			.BCC = ""
+			.Subject = "Sales Report"
+			.Body = "The attached report shows an update on your accounts."
+			.Attachments.Add ("C:\users\ezra\desktop\" & "Finance Report" & "\" & rep & ".xlsx")
+			'.Display
+			.Send
+		End With
+		On Error GoTo 0
+
+		Set OutMail = Nothing
+		Set OutApp = Nothing
+		
+	Loop Until Sheets("Reps").Cells(reprow, repcol) = ""
+
+	End If
+	
+End Sub
